045cff2efec211986a472fee4326d87f16080750
Jonatas Lizandro <jonatas.exe@gmail.com>
2021-11-13 11:33:28 -0300

    fix lint errors

--- footer/src/components/Footer.tsx
+++ footer/src/components/Footer.tsx
@@ -10,7 +10,7 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-const Logo = function (props: any) {
+const Logo = (props: any) => {
   return (
     <svg
       height={32}
@@ -30,7 +30,7 @@ const Logo = function (props: any) {
   );
 };
 
-const ListHeader = function ({ children }: { children: ReactNode }) {
+const ListHeader = ({ children }: { children: ReactNode }) => {
   return (
     <Text fontWeight="500" fontSize="lg" mb={2}>
       {children}
@@ -59,35 +59,35 @@ export default function LargeWithLogoLeft() {
           </Stack>
           <Stack align="flex-start">
             <ListHeader>Product</ListHeader>
-            <Link href="#">Overview</Link>
-            <Link href="#">Features</Link>
-            <Link href="#">Tutorials</Link>
-            <Link href="#">Pricing</Link>
-            <Link href="#">Releases</Link>
+            <Link href="/">Overview</Link>
+            <Link href="/">Features</Link>
+            <Link href="/">Tutorials</Link>
+            <Link href="/">Pricing</Link>
+            <Link href="/">Releases</Link>
           </Stack>
           <Stack align="flex-start">
             <ListHeader>Company</ListHeader>
-            <Link href="#">About</Link>
-            <Link href="#">Press</Link>
-            <Link href="#">Careers</Link>
-            <Link href="#">Contact</Link>
-            <Link href="#">Partners</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Press</Link>
+            <Link href="/">Careers</Link>
+            <Link href="/">Contact</Link>
+            <Link href="/">Partners</Link>
           </Stack>
           <Stack align="flex-start">
             <ListHeader>Support</ListHeader>
-            <Link href="#">Help Center</Link>
-            <Link href="#">Terms of Service</Link>
-            <Link href="#">Legal</Link>
-            <Link href="#">Privacy Policy</Link>
-            <Link href="#">Status</Link>
+            <Link href="/">Help Center</Link>
+            <Link href="/">Terms of Service</Link>
+            <Link href="/">Legal</Link>
+            <Link href="/">Privacy Policy</Link>
+            <Link href="/">Status</Link>
           </Stack>
           <Stack align="flex-start">
             <ListHeader>Follow Us</ListHeader>
-            <Link href="#">Facebook</Link>
-            <Link href="#">Twitter</Link>
-            <Link href="#">Dribbble</Link>
-            <Link href="#">Instagram</Link>
-            <Link href="#">LinkedIn</Link>
+            <Link href="/">Facebook</Link>
+            <Link href="/">Twitter</Link>
+            <Link href="/">Dribbble</Link>
+            <Link href="/">Instagram</Link>
+            <Link href="/">LinkedIn</Link>
           </Stack>
         </SimpleGrid>
       </Container>
